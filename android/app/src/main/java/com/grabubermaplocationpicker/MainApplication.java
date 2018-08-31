72da20747dfaddfeb0f56d4a221d84598b813f6a
son.bui <sonbui.gfd@gmail.com>
2018-09-01 02:23:12 +0700

    update example map location picker

--- android/app/src/main/java/com/grabubermaplocationpicker/MainApplication.java
+++ android/app/src/main/java/com/grabubermaplocationpicker/MainApplication.java
@@ -3,6 +3,8 @@ package com.grabubermaplocationpicker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new MapsPackage(),
+            new VectorIconsPackage()
       );
     }
 
